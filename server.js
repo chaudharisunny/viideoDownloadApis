68e39e39388c79447be2bd0d7eb26afb9b7beab7
chaudhari sunny <chaudharisunny6@gmail.com>
2025-09-08 16:01:17 +0530

    update code

--- server.js
+++ server.js
@@ -34,5 +34,5 @@ app.post('/api/v1/youtubepost', (req, res) => {
 });
 
 app.use("/downloads", express.static(path.join(__dirname, "downloads")));
-app.use('/api/v1',indexRoutes)
+app.use('/',indexRoutes)
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
