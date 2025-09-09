f8e79c27e9d7423b9d34cf293cfd71733ca5fed0
chaudhari sunny <chaudharisunny6@gmail.com>
2025-09-09 10:11:31 +0530

    update code

---
+++ render-build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -o errexit
+
+npm install
+
+# Install Playwright browsers
+npx playwright install --with-deps
+
+# Install yt-dlp binary
+mkdir -p bin
+curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o bin/yt-dlp
+chmod a+rx bin/yt-dlp
